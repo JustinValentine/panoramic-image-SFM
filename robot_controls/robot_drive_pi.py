ec6af53016cd4f67670bf04133e676498734f7b1
Justin Valentine <JustinValentineCS@gmail.com>
2023-04-10 16:43:14 -0600

    fixed?

--- robot_controls/robot_drive_pi.py
+++ robot_controls/robot_drive_pi.py
@@ -1,28 +1,57 @@
-import serial, struct, time, math
+import serial
+import struct
 import time
 from pyPS4Controller.controller import Controller
 
-
-LEFT_VELOCITY = 0 
-RIGHT_VELOCITY = 0 
-
 class MyPS4Controller(Controller):
-    def __init__(self, velocity_callback, **kwargs):
-        super(MyPS4Controller, self).__init__(**kwargs)
+    def __init__(self, interface, connecting_using_ds4drv, velocity_callback):
+        super(MyPS4Controller, self).__init__(interface=interface, connecting_using_ds4drv=connecting_using_ds4drv)
         self.left_velocity = 0
         self.right_velocity = 0
         self.prev_l3_value = 0
         self.velocity_callback = velocity_callback
+        self.connection = None
 
+        self.start_serial()
+        time.sleep(1)
+        self.listen()
+
+    def start_serial(self):
+        port = '/dev/ttyUSB0'
+        baud_rate = 115200
+        timeout = 1
+
+        try:
+            self.connection = serial.Serial(port, baudrate=baud_rate, timeout=timeout)
+            self.send_command_ascii('128')
+            self.send_command_ascii('131')
+            print("Connected!")
+        except:
+            print("Failed.")
+
+    def send_command_ascii(self, command):
+        cmd = bytes([int(v) for v in command.split()])
+        self.send_command_raw(cmd)
+
+    def send_command_raw(self, command):
+        try:
+            if self.connection is not None:
+                assert isinstance(command, bytes)
+                self.connection.write(command)
+                self.connection.flush()
+            else:
+                print("Not connected.")
+        except serial.SerialException:
+            print("Lost connection")
+            self.connection = None
 
     def update_wheel_velocities(self):
         self.left_velocity = max(min(int(self.left_velocity), 500), -500)
         self.right_velocity = max(min(int(self.right_velocity), 500), -500)
         self.velocity_callback(self.left_velocity, self.right_velocity)
 
-
     def on_L3_up(self, value):
-        pass  # Ignore L3 up/down events
+        pass
 
     def on_L3_down(self, value):
         pass
@@ -42,84 +71,33 @@ class MyPS4Controller(Controller):
         self.update_wheel_velocities()
 
     def on_R2_press(self, value):
-        # print('on_R2_press', (value / 32767) * 500)
         self.left_velocity = abs((value / 32767) * 500)
         self.right_velocity = abs((value / 32767) * 500)
         self.update_wheel_velocities()
 
     def on_L2_press(self, value):
-        # print('on_L2_press', (value / 32767) * 500)
-        self.left_velocity = -1*abs((value / 32767) * 500)
-        self.right_velocity = -1*abs((value / 32767) * 500)
+        self.left_velocity = -1 * abs((value / 32767) * 500)
+        self.right_velocity = -1 * abs((value / 32767) * 500)
         self.update_wheel_velocities()
 
+    def drive(self):
+        while True:
+            print('robot', self.left_velocity, self.right_velocity)
+            cmd = struct.pack(">Bhh", 145, self.left_velocity, self.right_velocity)
+            self.send_command_raw(cmd)
 
-def onStart():
-    global connection
-
-    port = '/dev/ttyUSB0' # Device name
-    Baudrate = 115200 # rate at which information is transferred in a communication channel
-    Timeout = 1 # Set a read timeout value in seconds
-
-    try:
-        connection = serial.Serial(port, baudrate=Baudrate, timeout=Timeout)
-        sendCommandASCII('128') # Send start command
-        sendCommandASCII('131') # Send command to Enter FULL mode 
-        print("Connected!")
-
-    except:
-        print("Failed.")
+            sleep_duration = 1 / 115200
+            time.sleep(sleep_duration)
 
 
-def sendCommandASCII(command):
-    cmd = bytes([int(v) for v in command.split()]) # An empty bytes object of the specified sizes will be created
-    sendCommandRaw(cmd)
-
-
-def sendCommandRaw(command):
-    global connection
-
-    try:
-        if connection is not None:
-            assert isinstance(command, bytes) # Check if commad is of type bytes 
-            connection.write(command) # Write the bytes data to the port
-            connection.flush()
-        else:
-            print("Not connected.")
-
-    except serial.SerialException:
-        print("Lost connection")
-        connection = None
-
-
-def Drive():
-    global LEFT_VELOCITY, RIGHT_VELOCITY
-
-    def velocity_callback(left_velocity, right_velocity):
-        global LEFT_VELOCITY, RIGHT_VELOCITY
-        LEFT_VELOCITY = left_velocity
-        RIGHT_VELOCITY = right_velocity
-
-    controller = MyPS4Controller(interface="/dev/input/js0", connecting_using_ds4drv=False, velocity_callback=velocity_callback)
-    controller.listen()
-
-    while True:
-        print('robot', LEFT_VELOCITY, RIGHT_VELOCITY)
-        cmd = struct.pack(">Bhh", 145, LEFT_VELOCITY, RIGHT_VELOCITY)  # Direct Drive 5 bytes little endian
-        sendCommandRaw(cmd)
-
-        baud_rate = 115200
-        sleep_duration = 1 / baud_rate
-
-        time.sleep(sleep_duration)
-
+def velocity_callback(left_velocity, right_velocity):
+    print('Robot:', left_velocity, right_velocity)
 
 
 def main():
-    onStart()
-    time.sleep(1)
-    Drive()
-
-
-if __name__ == "__main__":
-    main()
+    controller = MyPS4Controller(
+        interface="/dev/input/js0",
+        connecting_using_ds4drv=False,
+        velocity_callback=velocity_callback,
+    )
+    controller.drive
