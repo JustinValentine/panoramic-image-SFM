66677b283b5822403b53dce2c897d556e379486c
Justin Valentine <JustinValentineCS@gmail.com>
2023-04-09 19:24:36 -0600

    test

--- robot_controls/robot_drive_pi.py
+++ robot_controls/robot_drive_pi.py
@@ -1,48 +1,38 @@
 import time
-from ds4drv.backends import get_backend
-from ds4drv.controllers import BaseController
+from pyPS4Controller.controller import Controller
 
+class MyPS4Controller(Controller):
+    def __init__(self, **kwargs):
+        super(MyPS4Controller, self).__init__(**kwargs)
+        self.left_velocity = 0
+        self.right_velocity = 0
 
-class MyPS4Controller(BaseController):
-    def __init__(self, interface="/dev/input/js0", connecting_using_ds4drv=False):
-        backend = get_backend(connecting_using_ds4drv)
-        super(MyPS4Controller, self).__init__(backend, interface)
+    def on_L3_up(self, value):
+        pass  # Ignore L3 up/down events
 
+    def on_L3_down(self, value):
+        pass
 
-def handle_axes_event(event):
-    left_velocity = 0
-    right_velocity = 0
+    def on_L3_left(self, value):
+        self.left_velocity += value / 32767
+        self.right_velocity -= value / 32767
+        print(self.left_velocity, self.right_velocity)
 
-    # L3 to turn
-    if event.axis == BaseController.Axes.LEFT_ANALOG_X:
-        turn_speed = event.value  # Assuming event.value is in the range of -1 to 1
-        left_velocity += turn_speed
-        right_velocity -= turn_speed
+    def on_L3_right(self, value):
+        self.left_velocity -= value / 32767
+        self.right_velocity += value / 32767
+        print(self.left_velocity, self.right_velocity)
 
-    # R2 to move forward
-    if event.axis == BaseController.Axes.RIGHT_TRIGGER:
-        forward_speed = event.value  # Assuming event.value is in the range of 0 to 1
-        left_velocity += forward_speed
-        right_velocity += forward_speed
+    def on_R2_press(self, value):
+        self.left_velocity += value / 255
+        self.right_velocity += value / 255
+        print(self.left_velocity, self.right_velocity)
 
-    # L2 to move back
-    if event.axis == BaseController.Axes.LEFT_TRIGGER:
-        backward_speed = -event.value  # Assuming event.value is in the range of 0 to 1
-        left_velocity += backward_speed
-        right_velocity += backward_speed
+    def on_L2_press(self, value):
+        self.left_velocity -= value / 255
+        self.right_velocity -= value / 255
+        print(self.left_velocity, self.right_velocity)
 
-    print(left_velocity, right_velocity)
 
 controller = MyPS4Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.on_axes_event = handle_axes_event
-
-controller.start()
-time.sleep(0.1)
-
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
-finally:
-    controller.stop()
+controller.listen()
