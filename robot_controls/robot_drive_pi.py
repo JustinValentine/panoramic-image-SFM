c99b3d5e78b29dbb996358fc0703e93b0b0e4ad2
Justin Valentine <JustinValentineCS@gmail.com>
2023-04-09 19:53:28 -0600

    robot move?

--- robot_controls/robot_drive_pi.py
+++ robot_controls/robot_drive_pi.py
@@ -1,5 +1,10 @@
+import serial, struct, time, math
 import time
 from pyPS4Controller.controller import Controller
+
+
+LEFT_VELOCITY = 0 
+RIGHT_VELOCITY = 0 
 
 class MyPS4Controller(Controller):
     def __init__(self, **kwargs):
@@ -10,11 +15,17 @@ class MyPS4Controller(Controller):
 
 
     def update_wheel_velocities(self):
+        global LEFT_VELOCITY, RIGHT_VELOCITY 
         print('vl', self.left_velocity)
         print('vr', self.right_velocity)
-        left_velocity = max(min(self.left_velocity, 500), -500)
-        right_velocity = max(min(self.right_velocity, 500), -500)
-        print(left_velocity, right_velocity)
+        self.left_velocity = max(min(int(self.left_velocity), 500), -500)
+        self.right_velocity = max(min(int(self.right_velocity), 500), -500)
+
+        LEFT_VELOCITY = self.left_velocity 
+        RIGHT_VELOCITY = self.right_velocity
+
+        print(LEFT_VELOCITY, RIGHT_VELOCITY)
+
 
     def on_L3_up(self, value):
         pass  # Ignore L3 up/down events
@@ -49,5 +60,65 @@ class MyPS4Controller(Controller):
         self.update_wheel_velocities()
 
 
-controller = MyPS4Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen()
+def onStart():
+    global connection
+
+    port = '/dev/ttyUSB0' # Device name
+    Baudrate = 115200 # rate at which information is transferred in a communication channel
+    Timeout = 1 # Set a read timeout value in seconds
+
+    try:
+        connection = serial.Serial(port, baudrate=Baudrate, timeout=Timeout)
+        sendCommandASCII('128') # Send start command
+        sendCommandASCII('131') # Send command to Enter FULL mode 
+        print("Connected!")
+
+    except:
+        print("Failed.")
+
+
+def sendCommandASCII(command):
+    cmd = bytes([int(v) for v in command.split()]) # An empty bytes object of the specified sizes will be created
+    sendCommandRaw(cmd)
+
+
+def sendCommandRaw(command):
+    global connection
+
+    try:
+        if connection is not None:
+            assert isinstance(command, bytes) # Check if commad is of type bytes 
+            connection.write(command) # Write the bytes data to the port
+            connection.flush()
+        else:
+            print("Not connected.")
+
+    except serial.SerialException:
+        print("Lost connection")
+        connection = None
+
+
+def Drive():
+
+    controller = MyPS4Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
+    controller.listen()
+
+    while True:
+        cmd = struct.pack(">Bhh", 145, LEFT_VELOCITY, RIGHT_VELOCITY) # Drirect Drive 5 bytes little endian 
+        sendCommandRaw(cmd)
+
+
+        baud_rate = 115200
+        sleep_duration = 1 / baud_rate
+        
+        time.sleep(sleep_duration)
+
+
+def main():
+    onStart()
+    time.sleep(1)
+    Drive()
+
+
+if __name__ == "__main__":
+    main()
