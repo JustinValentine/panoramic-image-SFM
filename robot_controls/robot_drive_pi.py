c37ea3b067445a8d55d9198edb5cbcdfbb296f12
Justin Valentine <JustinValentineCS@gmail.com>
2023-04-09 19:21:21 -0600

    test

--- robot_controls/robot_drive_pi.py
+++ robot_controls/robot_drive_pi.py
@@ -1,114 +1,48 @@
-import serial, struct, time, math
-import numpy as np
-from pyPS4Controller.controller import Controller
 import time
-
-# These are the vendor and product IDs for a PS4 controller.
-VENDOR_ID = 0x054C
-PRODUCT_ID = 0x05C4
-
-def onStart():
-    global connection
-
-    port = '/dev/ttyUSB0'  # Device name
-    Baudrate = 115200  # rate at which information is transferred in a communication channel
-    Timeout = 1  # Set a read timeout value in seconds
-
-    try:
-        connection = serial.Serial(port, baudrate=Baudrate, timeout=Timeout)
-        sendCommandASCII('128')  # Send start command
-        sendCommandASCII('131')  # Send command to Enter FULL mode
-        print("Connected!")
-
-    except:
-        print("Failed.")
-
-def sendCommandASCII(command):
-    cmd = bytes([int(v) for v in command.split()])  # An empty bytes object of the specified sizes will be created
-    sendCommandRaw(cmd)
-
-def sendCommandRaw(command):
-    global connection
-
-    try:
-        if connection is not None:
-            assert isinstance(command, bytes)  # Check if commad is of type bytes
-            connection.write(command)  # Write the bytes data to the port
-            connection.flush()
-        else:
-            print("Not connected.")
-
-    except serial.SerialException:
-        print("Lost connection")
-        connection = None
-
-class MyPS4Controller(Controller):
-
-    def __init__(self, **kwargs):
-        Controller.__init__(self, **kwargs)
-        self.left_joystick_y = 0
-        self.r2_trigger = 0
-        self.l2_trigger = 0
-
-    def on_left_analog(self, x, y):
-        self.left_joystick_y = y
-
-    def on_R2_press(self, value):
-        self.r2_trigger = value
-
-    def on_L2_press(self, value):
-        self.l2_trigger = value
-
-    def send_wheel_commands(self):
-        vl, vr = calculate_wheel_velocities(self.left_joystick_y, self.r2_trigger, self.l2_trigger)
-        print(vl, vr)
-
-        cmd = struct.pack(">Bhh", 145, vl, vr)  # Direct Drive 5 bytes little endian
-        sendCommandRaw(cmd)
-
-        baud_rate = 115200
-        sleep_duration = 1 / baud_rate
-        time.sleep(sleep_duration)
+from ds4drv.backends import get_backend
+from ds4drv.controllers import BaseController
 
 
-def calculate_wheel_velocities(left_joystick_y, r2_trigger, l2_trigger, v_max=200):
-
-    left_joystick_y = (left_joystick_y - 127) / 127
-    r2_trigger /= 255
-    l2_trigger /= -255
-
-    linear_vel = v_max * r2_trigger + l2_trigger * v_max
-
-    if left_joystick_y < 0:
-        vl = linear_vel - (left_joystick_y * v_max)
-        vr = linear_vel
-
-    else:
-        vl = linear_vel
-        vr = linear_vel + (left_joystick_y * v_max)
-
-    print(int(vl), int(vr), left_joystick_y)
-
-    return int(vl), int(vr)
+class MyPS4Controller(BaseController):
+    def __init__(self, interface="/dev/input/js0", connecting_using_ds4drv=False):
+        backend = get_backend(connecting_using_ds4drv)
+        super(MyPS4Controller, self).__init__(backend, interface)
 
 
-def Drive():
+def handle_axes_event(event):
+    left_velocity = 0
+    right_velocity = 0
 
-    controller = MyPS4Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
-    controller.listen(timeout=1)
+    # L3 to turn
+    if event.axis == BaseController.Axes.LEFT_ANALOG_X:
+        turn_speed = event.value  # Assuming event.value is in the range of -1 to 1
+        left_velocity += turn_speed
+        right_velocity -= turn_speed
 
-    try:
-        while True:
-            controller.send_wheel_commands()
+    # R2 to move forward
+    if event.axis == BaseController.Axes.RIGHT_TRIGGER:
+        forward_speed = event.value  # Assuming event.value is in the range of 0 to 1
+        left_velocity += forward_speed
+        right_velocity += forward_speed
 
-    except KeyboardInterrupt:
-        controller.stop()
+    # L2 to move back
+    if event.axis == BaseController.Axes.LEFT_TRIGGER:
+        backward_speed = -event.value  # Assuming event.value is in the range of 0 to 1
+        left_velocity += backward_speed
+        right_velocity += backward_speed
 
-def main():
-    onStart()
-    time.sleep(1)
-    Drive()
+    print(left_velocity, right_velocity)
 
+controller = MyPS4Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.on_axes_event = handle_axes_event
 
-if __name__ == "__main__":
-    main()
+controller.start()
+time.sleep(0.1)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    controller.stop()
